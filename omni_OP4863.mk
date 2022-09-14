f7e4fa40d304c30783126fc16a0f8e58de2a7c86
Abhijith4124 <abhijith4124@gmail.com>
2022-09-14 23:19:00 +0530

    Initial Device Tree Files

---
+++ omni_OP4863.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from OP4863 device
+$(call inherit-product, device/oppo/OP4863/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := OP4863
+PRODUCT_NAME := omni_OP4863
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := CPH1969
+PRODUCT_MANUFACTURER := oppo
